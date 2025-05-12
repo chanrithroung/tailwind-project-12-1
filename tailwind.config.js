85f0d64b3671532d9edb8f0ce2464412b3140f98
Chanrith <chanrithroung2@gmail.com>
2025-05-12 13:55:29 +0700

    day1

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'banner': '#EFEFEF',
+        'rock': '#6A6B6A',
+      }
+    },
+  },
+  plugins: [],
+}
+
